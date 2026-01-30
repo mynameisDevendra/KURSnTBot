023ab3b4d08a7e10ae12b4808ac852884fe7d326
mynameisDevendra <devendra.sahu86@gmail.com>
2026-01-31 01:25:20 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# Install system dependencies (Poppler)
-# Note: This might require a custom Docker environment on some plans,
-# but try this standard build command first if you are on a standard environment.
-pip install --upgrade pip
-pip install -r requirements.txt
